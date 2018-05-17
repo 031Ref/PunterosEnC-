17bac55bad7ab0bde8df1a8792057d68bee42639
031Ref <martel.s.fernando@gmail.com>
2018-05-17 10:34:47 -0300

    nomeclatura de punteros usando enteros

--- nomeclatura/main.c
+++ nomeclatura/main.c
@@ -3,6 +3,35 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //punteros
+    //int* punteroNumero;
+    int *punteroNumero;
+
+    int numero;
+    int numeroDos;
+
+    numero = 66;
+
+    //&numeroDos=&numero; //ERORR
+    //numeroDos=&numero; //Asigna la memoria
+    punteroNumero=&numero;
+    numeroDos=*punteroNumero; //Asignacion de valor
+    //*punteroNumero=&numero; //ERROR
+    //*punteroNumero = 55; //ERROR
+
+    printf("%d\n",numero);
+    //printf("%d\n",&numero);
+    //printf("%d\n",punteroNumero);
+    //printf("%d\n",&punteroNumero);
+    printf("%p\n",&numero); //Lo muestra en hexadecimal
+    printf("%p\n",punteroNumero); //Lo muestra en hexadecimal
+    printf("%p\n",&punteroNumero); //Lo muestra en hexadecimal
+    printf("%d\n",*punteroNumero);
+    printf("%p\n",numeroDos);
+
+    /*
+    Una de las desventajas de usar punteros es que si no lo inicializamos, este apunta a cualquier lado
+    */
+
     return 0;
 }
